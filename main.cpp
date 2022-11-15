e946ef7ed10982b495ef4d00180073b45a0932b6
nmarie37 <nmarie37@users.noreply.github.com>
2022-11-14 19:47:55 -0500

    Latest Base

--- main.cpp
+++ main.cpp
@@ -1,24 +1,16 @@
 #include "stdafx.h"
-#include "datastore.h"
-#include "quizdesigner.h"
+#include "quizdesignerlatest.h"
 #include <QtWidgets/QApplication>
-#include <QDebug>
-#include <iostream>
 
-//datastore d;
-//extern datastore d;
+
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    QuizDesigner w;
+    //IntroDialog t;
+    //t.activateWindow();
+    QuizDesignerLatest w;
     w.show();
 
- 
-    //qDebug("name d is: %s", d.name);
-    //std::cout << "name1 d is: " << d.names[0] << "\n";
- 
-    
     return a.exec();
-
 }
